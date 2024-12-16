ca07d9116279d5fecc821b0f49f23aa81485b290
Jesus <jesuscta08@gmail.com>
2024-12-15 22:56:32 -0600

    Pruebas ngFor y ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
+import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-app';
+  title:string  = 'Hola mundo';
+
+  users:string[] = ['jesus', 'eduardo', 'moreno', 'larios'];
+  // users!:string[];
+
+  visible: boolean = false;
+  setVisible(): void{
+    this.visible = this.visible? false : true;
+    console.log('click en setVisible()')
+  }
 }
